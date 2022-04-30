561493dc106bc45f66c96b334271c76e6b01cef9
deacardos <102183309+deacardos@users.noreply.github.com>
2022-04-30 16:26:35 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,58 +1,77 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-unsigned int faStr1(const char *qwerty) {
- int i = 0, count = 0, in = 0, F = 0;
- while (qwerty[i] != '\0') {
-	 if (qwerty[i] == ' ' && (int)qwerty[i + 1] != 32 && (int)qwerty[i + 1] != '\0') {
-		 for (int f = i + 1; (int)qwerty[f] != 32; f++) {
-	 		if ((int)qwerty[f] > 47 && (int)qwerty[f] < 58)
-			 	F++;
-		}
-		 if (F == 0)
-			 in++;
-		 else
-			 F = 0;
-	 }
-	 i++;
- }
- return in;
+unsigned int faStr1(const char* str) {
+        int i = 0, schet = 0, Slovo = 0, Numer = 0;
+        while (str[i] != '\0') {
+                if (Slovo == 0 && str[i] != ' ') {
+                        if (str[i] > 57 || str[i] < 48) Numer = 1;
+                        Slovo = 1;
+                } else if (Slovo == 1 && str[i] != ' ' && Numer == 1) {
+                        if (str[i] >= 48 && str[i] <= 57) {
+                                Numer = 0;
+                        }
+                } else if (Slovo == 1 && str[i] == ' ') {
+                        if (Numer == 1) {
+                        schet++;
+                        }
+                        Numer = 0;
+                        Slovo = 0;
+                }
+                i++;
+        }
+        if (Slovo == 1 && Numer == 1) {
+                schet++;
+        }
+        return schet;
 }
-unsigned int faStr2(const char *qwerty) {
- int i = 0, count = 0, in = 0, F = 0;
- while (qwerty[i] != '\0')
- {
-	 if (qwerty[i] == ' ' && qwerty[i + 1] != ' ' && (int)qwerty[i + 1] < 91) {
-		 in++;
-	 }
-	 if (i == 0 && qwerty[i] != ' ') {
-		 for (int f = i; (int)qwerty[f] != 32; f++) {
-			 if ((int)qwerty[f] < 47)
-				 in--;
-		 }
-	 }
-	 i++;
- }
- count = in + 1;
- return count;
+unsigned int faStr2(const char* str) {
+        int i = 0, schet = 0, Slovo = 0, Number = 0;
+        while (str[i] != '\0') {
+                if (Slovo == 0 && str[i] >= 65 && str[i] <= 90) {
+                        Slovo = 1;
+                        Number = 1;
+                } else if (Slovo == 1 && str[i] != ' ' && Number == 1) {
+                        if (str[i] >= 65 && str[i] <= 90) {
+                                Number = 0;
+                        }
+                        if (str[i] > 122 || str[i] < 65) {
+                                Number = 0;
+                        }
+                } else if (Slovo == 1 && str[i] == ' ') {
+                        if (Number == 1) {
+                                schet++;
+                        }
+                        Number = 0;
+                        Slovo = 0;
+                }
+                i++;
+        }
+        if (Slovo == 1 && Number == 1) {
+                schet++;
+        }
+        return schet;
 }
-unsigned int faStr3(const char *qwerty) {
- int i = 0, count = 0, in = 0, F = 0;
- while (qwerty[i] != '\0') {
-	 if (qwerty[i] == ' ' && (int)qwerty[i + 1] != 32 && (int)qwerty[i + 1] != '\0') {
-		 for (int f = i + 1; (int)qwerty[f] != 32 && (int)qwerty[f] != '\0'; f++) {
-			 F++;
-		 }
-		 count++;
-	 }
-	 if (i == 0 && qwerty[0] != ' ') {
-		 for (int f = i; (int)qwerty[f] != 32; f++) {
-			 F++;
-		 }
-		 count++;
-	 }
-	 i++;
- }
- in = F / count;
- return in;
+unsigned int faStr3(const char* str) {
+        int i = 0, schet = 0, Slovo = 0, Leng = 0, Answer;
+        float Sum = 0;
+        while (str[i] != '\0') {
+                if (Slovo == 0 && str[i] != ' ') {
+                        schet++;
+                        Slovo = 1;
+                        Leng++;
+                } else if (Slovo == 1 && str[i] != ' ') {
+                        Leng++;
+                        } else if (Slovo == 1 && str[i] == ' ') {
+                        Sum = Sum + Leng;
+                        Slovo = 0;
+                        Leng = 0;
+                }
+                i++;
+        }
+        if (Slovo == 1) Sum = Sum + Leng;
+        Sum = Sum / static_cast<float>(schet);
+        Answer = Sum;
+        if (Sum - static_cast<float>(Answer) >= 0.5) Answer++;
+        return Answer;
 }
